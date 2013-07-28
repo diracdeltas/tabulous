af763a7511a22634d2fc8c8fd482744cfd04bcca
Yan <yan@mit.edu>
2013-07-28 23:56:23 +0200

    add info to panel

--- data/get-text.js
+++ data/get-text.js
@@ -1,8 +1,20 @@
 addon.port.on("show", function(myTabs) {
   var items = document.getElementById("items");
+  items.innerHTML = '';
   for (var i = 0; i < myTabs.length; i++) {
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(String(myTabs[i].url)));
+    var p = items.appendChild(document.createElement("p"));
+    p.id = 'check'+String(i);
+    var li = p.appendChild(document.createElement("input"));
+    li.type = "checkbox";
+    li.checked = true;
+    li.name = 'check'+String(i);
+    var url = p.appendChild(document.createElement("a"));
+    url.href = myTabs[i].url;
+    var title = url.appendChild(document.createTextNode(myTabs[i].title));
     items.appendChild(li);
+    li.onchange = function() {
+	    items.removeChild(document.getElementById(li.name));
+	    items.removeChild(li);
+    };
   }
 })
